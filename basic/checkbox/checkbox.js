c4f49c1fdbf11a791e6b1b87a5f0129112e36375
Dennis <dennis@Denniss-MacBook-Air.local>
2019-10-05 15:58:11 -0400

    update

--- basic/checkbox/checkbox.js
+++ basic/checkbox/checkbox.js
@@ -1,7 +1,22 @@
-function checkbox(checkbox, limit) {
-var checkbox = document.getElementsByClassName('checkbox')
-var limit = limit
-for (var i = 0; i < checkbox.length; i++)
-console.log(i)
+// function checking() {
+//     var checkbox = document.querySelectorAll("input:checked")
+//     var checkboxnumb = checkbox.length
+    
+//     if ()
+
+
+// //     for (var i = 0; i < checkboxnumb; i++)
+//         array.push(checkbox[i].values)
+//         console.log(array)
+// }
+
+// for (var i = 0; i < checkbox.length; i++){
+//  array.push(checkbox[i].value)
+ 
+// }
+function checking() {
+    var checkbox = document.querySelectorAll("input:checked")
+    for (var i = 0; i < checkbox.length; i++){
+        
+    }
 }
-checkbox(checkbox)
