0239fb1e3a40b6791950bdf852a3a94d2833cfe4
Jacklin1234 <lin.qicheng@yahoo.com>
2019-10-10 21:59:05 -0400

    update

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,62 +1,73 @@
-function myf(){
-    var btn = document.querySelector('#btn');
-    // console.log(text);
-    btn.addEventListener('click',function(){
-            // state.text = this.value;
-            // console.log(state.text);
-            var select = document.querySelector('select');
-            var preText = document.querySelector('h1').innerText;
-            var text;
-            if(select.value == 'Default')
-                text = 'Please Choose an Option';
-            else
-                text = preText + ' ' + select.value;
-            //console.log(text);
-            var div = document.querySelector('#show');
-            var textNode = document.createTextNode(text);
-            div.innerHTML = text;
-    })
-};
-function pop(){
-    var select = document.querySelector('select');
-    // console.log(text);
-    select.addEventListener('change',function(){
-            // state.text = this.value;
-            // console.log(state.text);
-            alert(event.target.value);
-    })
-}
-// function myh1(){
-//     var h1 = document.querySelector('h1');
-//     var text = state.text;
-//     var textNode = document.createTextNode(text);
-//     h1.innerHTML = text;
-// }
-
-// function myAjaxcall(url){
-//     return new Promise((resolve,reject)=> {
-//         resolve("Yes");
-//         reject("No");
-//     })
-// }
-// myAjaxcall().then(data => {
-//     console.log(data);
-// })
-// myf();
-// pop();
-
-// var x;
-// console.log(!!x);
-
-function x(){
-    var counter = 0;
-    return ()=>{
-        counter += 1;
-        console.log(counter);
+function myf(){
+        var btn = document.querySelector('#btn');
+        // console.log(text);
+        btn.addEventListener('click',function(){
+                // state.text = this.value;
+                // console.log(state.text);
+                var select = document.querySelector('select');
+                var preText = document.querySelector('h1').innerText;
+                var text;
+                if(select.value == 'Default')
+                    text = 'Please Choose an Option';
+                else
+                    text = preText + ' ' + select.value;
+                //console.log(text);
+                var div = document.querySelector('#show');
+                var textNode = document.createTextNode(text);
+                div.innerHTML = text;
+        })
+    };
+    function pop(){
+        var select = document.querySelector('select');
+        // console.log(text);
+        select.addEventListener('change',function(){
+                // state.text = this.value;
+                // console.log(state.text);
+                alert(event.target.value);
+        })
     }
-}
-
-const test = x();
-test();
-test();
-test();
+    // function myh1(){
+    //     var h1 = document.querySelector('h1');
+    //     var text = state.text;
+    //     var textNode = document.createTextNode(text);
+    //     h1.innerHTML = text;
+    // }
+    
+    // function myAjaxcall(url){
+    //     return new Promise((resolve,reject)=> {
+    //         resolve("Yes");
+    //         reject("No");
+    //     })
+    // }
+    // myAjaxcall().then(data => {
+    //     console.log(data);
+    // })
+    // myf();
+    // pop();
+    
+    // var x;
+    // console.log(!!x);
+    
+    function x(){
+        var counter = 0;
+        return ()=>{
+            counter += 1;
+            console.log(counter);
+        }
+    }
+    
+    const test = x();
+    // test();
+    // test();
+    // test();
+    console.log("" === false)
+    function add(a){
+        console.log(arguments.callee.name);
+        return (b) =>{
+            return (c) =>{
+                return a+b+c;
+            }
+        }
+    }
+    console.log(add(5)(6)(7));
+    
