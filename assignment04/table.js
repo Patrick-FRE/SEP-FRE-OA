c7e0b23d9d02ee957e82264a84543e005802f15b
Chaoran Chen <chosenchen@outlook.com>
2019-12-08 18:05:49 -0500

    add async/await

--- assignment04/table.js
+++ assignment04/table.js
@@ -1,30 +1,42 @@
 const table = document.querySelector('.content-table');
-const fetch = document.querySelector('.form-fetch');
+const open = document.querySelector('.form-fetch');
 const close = document.querySelector('.form-close');
 let post = null;
 
-function getData(cb){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           cb(JSON.parse(xhttp.response));
-        }
-    };
-    xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-    xhttp.send();
+async function getData(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    console.log(response);
+    let user = await response.json();
+    return user;
 }
 
-let p = new Promise((resolve, reject)=>{
-    getData(resolve);
-}).then(data => {
+getData().then(data => {
     post = data;
-    console.log(post);
-}).catch(err =>{
-    console.log(err);
-})
+    console.log(data)
+});
 
-fetch.addEventListener("click", e =>{
+// function getData(cb){
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//            // Typical action to be performed when the document is ready:
+//            cb(JSON.parse(xhttp.response));
+//         }
+//     };
+//     xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+//     xhttp.send();
+// }
+
+// let p = new Promise((resolve, reject)=>{
+//     getData(resolve);
+// }).then(data => {
+//     post = data;
+//     console.log(post);
+// }).catch(err =>{
+//     console.log(err);
+// })
+
+open.addEventListener("click", e =>{
     const html = `<tr>
         <th>id</th>
         <th>title</th>
@@ -39,9 +51,6 @@ fetch.addEventListener("click", e =>{
         <td>${element.body}</td>
     </tr>`;
     });
-
-    console.log(table);
-
 });
 
 close.addEventListener("click", e =>{
