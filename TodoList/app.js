6fd285762b180e69f2a700393177523e1168528d
patrick.lin@antra.com <patrick.lin@antra.com>
2019-12-09 15:49:33 -0500

    update

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,13 +1,135 @@
 console.log("app is working");
 
+// console.log("App is working");
+
+// // State
+// let userInput = "dfsfs";
+// let todoList = [];
+
+// // Model
+// let toDoIdCounter = 1;
+
+// class ToDo {
+//   constructor(content) {
+//     this.content = content;
+//     this.id = toDoIdCounter++;
+//   }
+//   generateTmp() {
+//     return `<li>${this.content} <button onclick="hanlderRemove(${this.id})">Remove</button></li>`;
+//   }
+// }
+
+// // DOM element
+// let userInputElement = document.querySelector(".inputBar");
+// let todoListElement = document.querySelector(".todoList");
+
+// // Setup Event
+// eventSetup();
+// function eventSetup() {
+//   userInputElement.addEventListener("keyup", handlerKeyup);
+// }
+
+// // UI Tmp
+// function generateTodoListTmp() {
+//   let resTmp = "";
+//   todoList.forEach(itemToDo => {
+//     resTmp += itemToDo.generateTmp();
+//   });
+//   return resTmp;
+// }
+// function render(rootElement, tmp) {
+//   rootElement.innerHTML = tmp;
+// }
+
+// // Event Handler
+// function handlerKeyup() {
+//   userInput = event.target.value;
+//   if (event.keyCode === 13) {
+//     console.log("enter");
+//     // update State
+//     addTodo(new ToDo(userInput));
+//     userInput = "";
+//     // update View
+//     render(todoListElement, generateTodoListTmp());
+//     userInputElement.value = userInput;
+//   }
+// }
+
+// function hanlderOnsubmit() {
+//   event.preventDefault();
+// }
+
+// function hanlderRemove(id) {
+//   console.log("remove");
+//   // update State
+//   removeTodo(id);
+//   // updaste View
+//   render(todoListElement, generateTodoListTmp());
+// }
+
+// // business logic
+
+// function addTodo(todo) {
+//   todoList.push(todo);
+// }
+
+// function removeTodo(id) {
+//   todoList = todoList.filter(itemTodo => {
+//     if (itemTodo.id === id) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   });
+// }
+
 // Model
 const Model = (function() {
-    class Todo{
-        constructor(content)
+  let state = {
+    userInput: "",
+    todoList: []
+  };
+
+  let toDoIdCounter = 1;
+  class ToDo {
+    constructor(content) {
+      this.content = content;
+      this.id = toDoIdCounter++;
     }
+    generateTmp() {
+      return `<li>${this.content} <button onclick="Controller.hanlderRemove(${this.id})">Remove</button></li>`;
+    }
+  }
 
+  function generateTodoListTmp() {
+    let resTmp = "";
+    state.todoList.forEach(itemToDo => {
+      resTmp += itemToDo.generateTmp();
+    });
+    return resTmp;
+  }
 
-  return {};
+  function addTodo(todo) {
+    state.todoList.push(todo);
+  }
+
+  function removeTodo(id) {
+    state.todoList = state.todoList.filter(itemTodo => {
+      if (itemTodo.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
+  return {
+    state,
+    addTodo,
+    removeTodo,
+    Todo: content => new ToDo(content),
+    generateTodoListTmp
+  };
 })();
 
 // View
@@ -15,23 +137,61 @@ const Model = (function() {
 const View = (function() {
   const DOMString = {
     inputBar: ".inputBar",
-    todoList: ".todoList"
+    todoList: ".todoList",
+    inputForm: ".inputForm"
   };
 
+  function render(rootElement, tmp) {
+    rootElement.innerHTML = tmp;
+  }
+
   return {
-    DOMString
+    DOMString,
+    render
   };
 })();
 
 // Controller
 
-const Controller = (function(view) {
+const Controller = (function(view, model) {
   function eventSetup() {
     document
       .querySelector(view.DOMString.inputBar)
-      .addEventListener("keyup", () => {
-        console.log("keyup");
-      });
+      .addEventListener("keyup", handlerKeyup);
+    document
+      .querySelector(view.DOMString.inputForm)
+      .addEventListener("submit", hanlderOnsubmit);
+  }
+  function hanlderRemove(id) {
+    console.log("remove");
+    // update State
+    model.removeTodo(id);
+    // updaste View
+    view.render(
+      document.querySelector(view.DOMString.todoList),
+      model.generateTodoListTmp()
+    );
+  }
+
+  function hanlderOnsubmit() {
+    event.preventDefault();
+  }
+
+  function handlerKeyup() {
+    model.state.userInput = event.target.value;
+    if (event.keyCode === 13) {
+      console.log("enter");
+      // update State
+      model.addTodo(model.Todo(model.state.userInput));
+      model.state.userInput = "";
+      // update View
+      view.render(
+        document.querySelector(view.DOMString.todoList),
+        model.generateTodoListTmp()
+      );
+      document.querySelector(view.DOMString.inputBar).value =
+        model.state.userInput;
+    }
   }
 
   function init() {
@@ -40,8 +200,9 @@ const Controller = (function(view) {
   }
 
   return {
-    init
+    init,
+    hanlderRemove
   };
-})(View);
+})(View, Model);
 
 Controller.init();
