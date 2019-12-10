ea7adfcbea40aca153db880d484172caa04f91dc
Samuel Han <realsamuelhan95@gmail.com>
2019-12-09 22:17:09 -0500

    2nd commit, added clr-btn, beautified table

--- JS/JS-Fetch-Assignment/index.js
+++ JS/JS-Fetch-Assignment/index.js
@@ -1,1 +1,24 @@
+function fetchData() {
+  var element = document.getElementById("data");
+  fetch("https://jsonplaceholder.typicode.com/posts").then(res => {
+    res.json().then(data => {
+      console.log(data);
+      if (data.length > 0) {
+        var temp = "";
+        data.forEach(u => {
+          temp += "<tr>";
+          temp += "<td>" + u.userId + "</td>";
+          temp += "<td>" + u.title + "</td>";
+          temp += "<td>" + u.body + "</td></tr>";
+        });
+        document.getElementById("data").innerHTML = temp;
+      }
+    });
+  });
+  element.classList.remove("newClass");
+}
 
+function clearData() {
+  var element = document.getElementById("data");
+  element.classList.add("newClass");
+}
