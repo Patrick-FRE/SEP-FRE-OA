de2e92a633cbbe6dab52a20a6a4ff0e88c5b5bb3
jyang82 <jyang82@binghamton.edu>
2020-02-03 12:55:04 -0500

    js

--- jia_js_assignment/js_assignment.js
+++ jia_js_assignment/js_assignment.js
@@ -329,7 +329,16 @@ function notRepeat(str) {
 
 //24.
 function bubbleSort(arr) {
-
+    for(let i=0; i<arr.length; i++) {
+        for(let j=0; j<arr.length-i; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 
@@ -357,7 +366,9 @@ function longestNoRepeat(str) {
 
 
 //27.
-
+function longestPalindrome(str) {
+    
+}
 
 
 
