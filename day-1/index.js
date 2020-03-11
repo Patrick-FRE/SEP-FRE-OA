45f6862377f021afe8afa73c3ec3f60c2797f27c
Patrick-FRE <patrick.lin@antra.com>
2020-03-11 15:06:56 -0400

    update

--- day-1/index.js
+++
@@ -1,8 +0,0 @@
-console.log("hello everyone");
-console.log(this);
-
-function main() {
-  console.log(this);
-}
-
-main();
--- day-2/index.js
+++
@@ -1,6 +0,0 @@
-localStorage.setItem("name", "patrick");
-var cat = localStorage.getItem("myCat");
-
-window.addEventListener("DOMContentLoaded", event => {
-  console.log("DOM fully loaded and parsed");
-});
