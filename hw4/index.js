336b0874d08de629a95b57012dd526321783aa0e
dean <deanthechung@gmail.com>
2020-03-16 11:14:07 -0400

    added toggle ability

--- hw4/index.js
+++ hw4/index.js
@@ -1,29 +1,24 @@
-const show = document.querySelector('.get-todos')
+const getButton = document.querySelector('.get-todos')
 const deleteButton = document.querySelector('.delete-todos')
 const list = document.querySelector('.todos')
 
 const getTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(response=> {
-        return response.json()
-    }).then(data => {
-        if (Array.isArray(data)) {
+    .then(response=> response.json())
+    .then(data => {
         for (let i = 0; i < data.length; i++) {
             const z = document.createElement('li');
             z.innerHTML = `${data[i].id}: ${data[i].title}`;
             list.appendChild(z);
         }
-    }
-    else {
-        const z = document.createElement('li');
-        z.innerHTML = `${data.id}: ${data.title}`;
-        list.appendChild(z);
-    }
     }).catch(err => {
         console.log('error:', err);
     })
 }
-show.addEventListener('click', getTodos)
+getButton.addEventListener('click', getTodos)
+
 deleteButton.addEventListener('click', () => {
-    document.querySelector('ul').parentElement.removeChild(document.querySelector('ul'))
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
 })
