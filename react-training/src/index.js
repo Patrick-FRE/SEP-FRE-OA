551581e2fd0da952ac34918bb0ad884bb35986f9
Patrick-FRE <patrick.lin@antra.com>
2020-03-25 09:37:27 -0400

    update

--- react-training/src/index.js
+++ react-training/src/index.js
@@ -4,12 +4,32 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: true
+    };
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onClickHandler() {
+    this.setState({
+      isShow: false
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.onClickHandler}>Remove</button>
+        {this.state.isShow ? <App></App> : "Hide"}
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ react-training/src/test.js
@@ -0,0 +1,10 @@
+class App {
+  render = () => {
+    console.log("hello");
+    return "hello world";
+  };
+}
+
+let a = new App();
+
+a.render();
