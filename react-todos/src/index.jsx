5c4fd67564c1cbd43a547b355300d479fbdbe73e
dean <deanthechung@gmail.com>
2020-03-30 09:19:18 -0700

    own redux implementation

--- react-todos/src/index.jsx
+++ react-todos/src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,7 +10,41 @@ import thunk from 'redux-thunk';
 import styles from './scss/application.scss';
 import reducers from './reducers';
 
+
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+// store.dispatch({ type: 'ADD_TODO', todo: 'read' })
+// store.dispatch({ type: 'ADD_TODO', todo: 'bike' })
+
+// const delayRemove = (todo) => {
+//   return (dispatch) => {
+//     setTimeout(() => {
+//       dispatch({type:'DELETE_TODO',todo})
+//     },1000)
+//   }
+// }
+// class ReduxApp extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.forceUpdate = this.forceUpdate.bind(this)
+//   }
+//   componentDidMount() {
+//     this.props.store.subscribe(() => {
+//       return this.forceUpdate()
+//     })
+//   }
+//   render() {
+//     console.log('this.props.store', this.props.store)
+//     const items = this.props.store.getState().todos.map((todo, i) => {
+//       return <li key={i}>{todo}<span><button onClick={() => { this.props.store.dispatch(delayRemove(todo)) }}>remove</button></span></li>
+//     })
+//     return (
+//       <div><ul>{items}</ul></div>
+//     )
+
+//   }
+// }
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
